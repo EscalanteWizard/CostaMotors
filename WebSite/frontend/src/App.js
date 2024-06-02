6a33d5489d421437aca251c42e7de125a4841ecc
EscalanteWizard <jordano.shordi@gmail.com>
2024-06-02 14:06:48 -0600

    consultar empleados

--- WebSite/frontend/src/App.js
+++ WebSite/frontend/src/App.js
@@ -2,9 +2,11 @@ import './App.css';
 import {useState} from "react";
 import React from 'react';
 import Axios from "axios";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
+  //Constantes para manejar los estados de los input en el formulario
   const [nombre,setNombre] = useState("");
   const [edad,setEdad] = useState(0);
   const [pais,setPais] = useState("");
@@ -47,13 +49,13 @@ function App() {
         } else {
           alert("No se encontraron empleados.");
         }
-      })
-      .catch((error) => {
+      }).catch((error) => {
         console.error("Error al recuperar la lista de empleados: ", error);
         alert("Error al recuperar la lista de empleados");
       });
   };
 
+  /** Funcion para testear que la conexion con la base de datos se haya realizado con exito */
   const testConexion = ()=>{
     Axios.get("http://localhost:3007/testConnection",{
   
@@ -62,55 +64,105 @@ function App() {
     });
   }
 
+  //Renderizado de los componentes graficos de la aplicacion
   return (
-    //Aplicacion
-    <div className="App">
-      <div className="datos">
-        {/* Etiqueta para el nombre */}
-        <label>Nombre: <input 
-        onChange={(event)=>{
-          setNombre(event.target.value);
-        }}
-        type="text"/></label>
-        {/* Etiqueta para la edad */}
-        <label>Edad: <input 
-        onChange={(event)=>{
-          setEdad(event.target.value);
-        }}
-        type="number"/></label>
-        {/* Etiqueta para el pais */}
-        <label>Pais: <input onChange={(event)=>{
-          setPais(event.target.value);
-        }}
-        type="text"/></label>
-        {/* Etiqueta para el cargo */}
-        <label>Cargo: <input 
-        onChange={(event)=>{
-          setCargo(event.target.value);
-        }}
-        type="text"/></label>
-        {/* Etiqueta para la experiencia */}
-        <label>Experiencia: <input 
-        onChange={(event)=>{
-          setExperiencia(event.target.value);
-        }}
-        type="number"/></label>
-        {/* Boton para registrar */}
-        <button onClick={addEmpleado}>Registrar</button>
-      </div>
-      <div className="listaEmpleados">
-        {/* Al precionar el boton de listar empleados, por cada empleado encontrado crea un div con la infromación de dicho empleado */}
-      {
-        <React.Fragment>
-        {empleadosList.map((empleado, index) => (
-          <div key={index} className='empleado'>
-            Nombre: {empleado.nombre}, Edad: {empleado.edad}, País: {empleado.pais}, Cargo: {empleado.cargo}, Experiencia: {empleado.experiencia}
+    <div className="container">
+      <div className="card text-center">
+        <div className="card-header">
+          Gestión de empleados
+        </div>
+        {/**Card es un elementos de bootstrap para mostrar elementos de forma organizada*/}
+        <div className="card-body">
+
+          {/**Input group es un elemento de bootstrap para gestionar elementos para ingresar datos 
+           * En este se muestran los elementos para ingresar el nombre de empleado*/}
+          <div className="input-group mb-3">
+            <span className="input-group-text" id="basic-addon1">Nombre:</span>
+            <input type="text" 
+              onChange={(event)=>{
+              setNombre(event.target.value);
+            }}
+            className="form-control" placeholder="Ingrese un nombre" aria-label="Username" aria-describedby="basic-addon1"/>
           </div>
-        ))}
-      </React.Fragment>
-      
-      }
+
+          {/**Input group es un elemento de bootstrap para gestionar elementos para ingresar datos 
+           * En este se muestran los elementos para ingresar a edad del empleado*/}
+          <div className="input-group mb-3">
+            <span className="input-group-text" id="basic-addon1">Edad:</span>
+            <input type="number" 
+              onChange={(event)=>{
+              setEdad(event.target.value);
+            }}
+            className="form-control" placeholder="Ingrese la edad" aria-label="Age" aria-describedby="basic-addon1"/>
+          </div>
+
+          {/**Input group es un elemento de bootstrap para gestionar elementos para ingresar datos 
+           * En este se muestran los elementos para ingresar el país del empleado*/}
+          <div className="input-group mb-3">
+            <span className="input-group-text" id="basic-addon1">País:</span>
+            <input type="text" 
+              onChange={(event)=>{
+              setPais(event.target.value);
+            }}
+            className="form-control" placeholder="Ingrese el país" aria-label="Country" aria-describedby="basic-addon1"/>
+          </div>
+          
+          {/**Input group es un elemento de bootstrap para gestionar elementos para ingresar datos 
+           * En este se muestran los elementos para ingresar el cargo del empleado*/}
+          <div className="input-group mb-3">
+            <span className="input-group-text" id="basic-addon1">Cargo:</span>
+            <input type="text" 
+              onChange={(event)=>{
+              setCargo(event.target.value);
+            }}
+            className="form-control" placeholder="Ingrese el cargo" aria-label="Position" aria-describedby="basic-addon1"/>
+          </div>
+          
+          {/**Input group es un elemento de bootstrap para gestionar elementos para ingresar datos 
+           * En este se muestran los elementos para ingresar la experiencia del empleado*/}
+          <div className="input-group mb-3">
+            <span className="input-group-text" id="basic-addon1">Experiencia:</span>
+            <input type="number" 
+              onChange={(event)=>{
+              setExperiencia(event.target.value);
+            }}
+            className="form-control" placeholder="Ingrese la experiencia" aria-label="Position" aria-describedby="basic-addon1"/>
+          </div>
+        </div>
+
+        <div className="card-footer text-muted">
+          {/* Boton para registrar */}
+          <button className='btn btn-success' onClick={addEmpleado}>Registrar</button>
+        </div>
       </div>
+      {/**Tabla para mostrar los resultados de los Empleados consultados */}
+      <table className="table table-striped">
+        <thead>
+          {/**Aquí se crean las columnas de la tabla donde aparecen los resultados de la consulta */}
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Edad</th>
+            <th scope="col">Pais</th>
+            <th scope="col">Cargo</th>
+            <th scope="col">Experiencia</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/**Aquí se define que en los campos de la tabla de consultas aparecerán los atributos de cada empleado consultado */}
+          {empleadosList.map((empleado, index) => { 
+            return <tr key={empleado.id}>
+                    <th>{empleado.id}</th>
+                    <td>{empleado.nombre}</td>
+                    <td>{empleado.edad}</td>
+                    <td>{empleado.pais}</td> 
+                    <td>{empleado.cargo}</td> 
+                    <td>{empleado.experiencia}</td> 
+                  </tr>
+            })
+          }
+        </tbody>
+      </table>
     </div>
   );
 }
